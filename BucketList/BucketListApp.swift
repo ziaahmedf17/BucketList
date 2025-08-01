e0d72a51637991d5fb50bd2507e999e4527a4323
Zia Ahmed <ziaahmedf17@gmail.com>
2025-08-02 02:42:36 +0500

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Zi on 02/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
