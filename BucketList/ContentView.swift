63ca620fe76152f84b878a4e3bacc04e35fddd13
Zia Ahmed <ziaahmedf17@gmail.com>
2025-08-02 05:32:16 +0500

    Update ContentView.swift

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -5,17 +5,24 @@
 //  Created by Zi on 02/08/2025.
 //
 
+import MapKit
 import SwiftUI
 
+struct Location: Identifiable{
+    let id = UUID()
+    let name: String
+    let coordinate: CLLocationCoordinate2D
+}
+
 struct ContentView: View {
+    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D( latitude: 51.5, longitude: -0.12), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+    let locations:[
+        Location(nam)
+    ]
+        
+    }
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Map(coordinateRegion: $mapRegion)
     }
 }
 
