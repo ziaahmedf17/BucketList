40fc6245bad4b481151d83ca261c217ae59893e9
Zia Ahmed <ziaahmedf17@gmail.com>
2025-08-02 05:53:06 +0500

    Update ContentView.swift

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -16,13 +16,26 @@ struct Location: Identifiable{
 
 struct ContentView: View {
     @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D( latitude: 51.5, longitude: -0.12), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
-    let locations:[
-        Location(nam)
+    let locations = [
+        Location(name: "Zi Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.1412)),
+        Location(name: "Ahm of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
     ]
         
-    }
     var body: some View {
-        Map(coordinateRegion: $mapRegion)
+        NavigationView{
+            Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
+                MapAnnotation(coordinate: location.coordinate){
+                    NavigationLink{
+                        Text(location.name)
+                    } label: {
+                        Circle()
+                            .stroke(.red, lineWidth: 3)
+                            .frame(width: 44, height: 44)
+                    }
+                }
+            }
+        }
+        .navigationTitle("W Explored")
     }
 }
 
